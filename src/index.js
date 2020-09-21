73208f5120710de911c50f649b500eba483890d1
Andrew <chuntcdj@gmail.com>
2020-09-21 11:28:33 +0800

    Commiting the frontend code of tiktok clone

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+
+// FRONTEND                         BACKEND
+// 1. ReactJS                       1. Node.js
+// 2. Material UI                   2. Express.js
+// 3. Flexbox                       3. MongoDB + Mongoose
+// 4. 3rd Party Ticker              4. Heroku Hosting
+// 5. Scroll Snap
+// 6. Firebase Hosting
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
